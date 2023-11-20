14f020482fad5b70fa055998d4908436dbe9d029
nero <hungnd.vnse@gmail.com>
2023-11-20 16:06:52 +0700

    [feat] implement Stage 2/5: Add functionality

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,11 +1,37 @@
 package org.hyperskill.stopwatch
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import org.hyperskill.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var count = 0
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.startButton.setOnClickListener {
+            handler.removeCallbacks(startCounting)
+            handler.postDelayed(startCounting, 1000)
+        }
+        binding.resetButton.setOnClickListener {
+            handler.removeCallbacks(startCounting)
+            count = 0
+            binding.textView.text = "00:00"
+        }
+    }
+
+    private val startCounting = object : Runnable {
+        override fun run() {
+            count++
+            binding.textView.text = "${String.format("%02d", count / 60)}:${String.format("%02d", count % 60)}"
+            handler.postDelayed(this, 1000)
+        }
     }
 }
