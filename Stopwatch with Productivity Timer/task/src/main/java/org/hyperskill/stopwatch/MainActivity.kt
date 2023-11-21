f1e14d90fccf3181819a2372e47a1396c1351d0f
nero <hungnd.vnse@gmail.com>
2023-11-21 22:21:08 +0700

    [feat] implement Stage 5/5: Friendly reminder.

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.hyperskill.stopwatch
 
 import android.app.AlertDialog
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Build
@@ -11,6 +15,7 @@ import android.view.View
 import android.widget.EditText
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import org.hyperskill.stopwatch.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -21,9 +26,36 @@ class MainActivity : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
     private var limit: Int? = null
 
+    private val channelId = "org.hyperskill"
+    private val name = "Stopwatch"
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private val importance = NotificationManager.IMPORTANCE_HIGH
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val channel = NotificationChannel(channelId, name, importance)
+
+    private val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle("Stopwatch")
+        .setContentText("Time exceeded")
+        .setOnlyAlertOnce(true)
+
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notification: Notification
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        // Register the channel with the system
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        notification = notificationBuilder.build().apply {
+            flags = flags or Notification.FLAG_INSISTENT
+        }
+
         binding.startButton.setOnClickListener {
             handler.removeCallbacks(startCounting)
             binding.progressBar.visibility = View.VISIBLE
@@ -57,16 +89,21 @@ class MainActivity : AppCompatActivity() {
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         override fun run() {
             count++
+            limit?.let {
+                if (it in 1 until count) {
+                    binding.textView.setTextColor(Color.RED)
+                    notificationManager.notify(
+                        393939,
+                        notification
+                    )
+                }
+            }
             val color = Random.nextInt()
             binding.progressBar.indeterminateTintList = ColorStateList.valueOf(color)
             binding.textView.text =
                 "${String.format("%02d", count / 60)}:${String.format("%02d", count % 60)}"
+
             handler.postDelayed(this, 1000)
-            limit?.let {
-                if (count > it) {
-                    binding.textView.setTextColor(Color.RED)
-                }
-            }
         }
     }
 }
